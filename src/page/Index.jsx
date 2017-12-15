310ccb183e47f0bc8f0c3cb7c33452e210887718
zengze <zengze@smart>
2017-12-15 11:40:10 +0800

    添加基础文件

---
+++ src/page/Index.jsx
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+
+import { TabBar } from 'antd-mobile';
+
+import * as containers from '../containers/index.jsx';
+
+export default class Index extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'homeTab',
+    };
+  }
+
+  render() {
+
+    return (
+      <div style={{ position: 'fixed', height: '100%', width: '100%', top: 0 }}>
+        <TabBar
+          unselectedTintColor="#949494"
+          tintColor="#33A3F4"
+          barTintColor="white">
+          <TabBar.Item
+            key="home"
+            title="首页"
+            icon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            selectedIcon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            selected={this.state.selectedTab === 'homeTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'homeTab',
+              });
+            }}
+          >
+            <containers.Home />
+          </TabBar.Item>
+          <TabBar.Item
+            key="work"
+            title="工作"
+            icon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://zos.alipayobjects.com/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            selectedIcon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://zos.alipayobjects.com/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            selected={this.state.selectedTab === 'workTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'workTab',
+              });
+            }}
+            badge={5}
+          >
+            <containers.Work />
+          </TabBar.Item>
+          <TabBar.Item
+            key="mine"
+            title="我的"
+            icon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            selectedIcon={
+              <div style={{
+                width: '22px',
+                height: '22px',
+                background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat' }}
+              />
+            }
+            selected={this.state.selectedTab === 'mineTab'}
+            onPress={() => {
+              this.setState({
+                selectedTab: 'mineTab',
+              });
+            }}
+          >
+            <containers.Mine />
+          </TabBar.Item>
+        </TabBar>
+      </div>
+    );
+  }
+}
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common',
+      filename: 'common.js'
+    }),
+    new ExtractTextPlugin({ filename: 'common.css', allChunks: true })
+  ],
+  entry: {
+    index: './src/entry/index.jsx'
+  },
+  output: {
+    path: path.join(__dirname, '/public/dist'),
+    filename: '[name].js'
+  },
+
+  resolve: {
+    modules: [path.resolve(__dirname, 'node_modules'), path.join(__dirname, 'src')],
+    extensions: ['.web.js', '.jsx', '.js', '.json'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/, exclude: /node_modules/, loader: 'babel-loader',
+        options: {
+          plugins: [
+            'external-helpers', // why not work?
+            ["transform-runtime", { polyfill: false }],
+            ["import", [{ "style": "css", "libraryName": "antd-mobile" }]]
+          ],
+          presets: ['es2015', 'stage-0', 'react']
+          // presets: [['es2015', { modules: false }], 'stage-0', 'react'] // tree-shaking
+        }
+      },
+      { test: /\.(jpg|png)$/, loader: "url-loader?limit=8192" },
+      {
+        test: /\.(svg)$/i, loader: 'svg-sprite-loader', include: [
+          require.resolve('antd-mobile').replace(/warn\.js$/, ''),  // 1. 属于 antd-mobile 内置 svg 文件
+          // path.resolve(__dirname, 'src/my-project-svg-foler'),  // 自己私人的 svg 存放目录
+        ]
+      },
+      // 注意：如下不使用 ExtractTextPlugin 的写法，不能单独 build 出 css 文件
+      // { test: /\.less$/i, loaders: ['style-loader', 'css-loader', 'less-loader'] },
+      // { test: /\.css$/i, loaders: ['style-loader', 'css-loader'] },
+      {
+        test: /\.less$/i, use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader', 'less-loader'
+          ]
+        })
+      },
+      {
+        test: /\.css$/i, use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader'
+          ]
+        })
+      }
+    ]
+  },
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  },
+}
